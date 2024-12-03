ccadd4edeb456b99f7140ffb0136d773cb779e4e
Amarjit <121117087+username-amarjit@users.noreply.github.com>
2024-12-03 16:54:29 +0000

    added code for parser

--- compiler/code/compiler.js
+++ compiler/code/compiler.js
@@ -4,7 +4,7 @@ function tokenizer(input_code) {
     let cursor = 0;
     let tokens = [];
 
-    while (cursor<input_code.length) {
+    while (cursor < input_code.length) {
         char = input_code[cursor];
 
         if (char === "(" || char === ")") {
@@ -52,7 +52,7 @@ function tokenizer(input_code) {
             }
             cursor++
             tokens.push({
-                type: "String",
+                type: "string",
                 value: word,
             });
             continue;
@@ -63,6 +63,7 @@ function tokenizer(input_code) {
     return tokens;
 }
 
+const { type } = require("os");
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -73,6 +74,131 @@ const rl = readline.createInterface({
 rl.question("Enter something: ", (input) => {
     const out = tokenizer(input)
     console.log(out)
+    const out2 = parser(out)
+    console.log(out2)
 
     rl.close();
 });
+
+
+// function parser(tokens) {
+
+//     let cursor = 0;
+
+//     let token = tokens[cursor]
+
+//     function walk() {
+//         if (token.type == 'number') {
+//             cursor++
+//             return {
+//                 type: "numberLiteral",
+//                 value: token.value
+//             }
+//         }
+
+//         if (token.type == 'string') {
+//             cursor++
+//             return {
+//                 type: "stringLiteral",
+//                 value: token.value
+//             }
+//         }
+//         if (token.type === 'paren' &&
+//             token.value === '(') {
+//             token = tokens[++cursor]
+
+//             let node = {
+//                 type: "CallExpression",
+//                 value: token.value,
+//                 parmas: []
+//             }
+//             token = tokens[++cursor]
+
+//             while (token.type !== 'paren' || (token.type === 'paren' &&
+//                 token.value !== ')')) {
+//                 node.parmas.push(walk())
+//                 token = tokens[++cursor]
+//             }
+
+//             cursor++
+//             return node
+
+//         }
+
+//         throw new TypeError("Unknown token type: " + token.type); // Handling unknown tokens
+
+//     }
+
+//     let ast = {
+//         type: "Program",
+//         body: []
+//     }
+
+//     while (cursor < tokens.length) {
+//         ast.body.push(walk())
+//     }
+
+
+// return ast
+// }
+
+function parser(tokens) {
+    let cursor = 0;
+    let token = tokens[cursor];
+
+    function walk() {
+        if (token.type === 'number') {
+            cursor++;
+            token = tokens[cursor];
+            return {
+                type: "NumberLiteral",
+                value: token.value
+            };
+        }
+
+        if (token.type === 'string') {
+            cursor++;
+            token = tokens[cursor];
+            return {
+                type: "StringLiteral",
+                value: token.value
+            };
+        }
+
+        if (token.type === 'paren' && token.value === '(') {
+            cursor++;
+            token = tokens[cursor];
+
+            let node = {
+                type: "CallExpression",
+                name: token.value, // Call function name is usually in the next token
+                params: []
+            };
+
+            cursor++;
+            token = tokens[cursor];
+
+            while (token.type !== 'paren' || (token.type === 'paren' && token.value !== ')')) {
+                node.params.push(walk());
+                token = tokens[cursor];
+            }
+
+            cursor++;
+            token = tokens[cursor];
+            return node;
+        }
+
+        throw new TypeError("Unknown token type: " + token.type); // Handling unknown tokens
+    }
+
+    let ast = {
+        type: "Program",
+        body: []
+    };
+
+    while (cursor < tokens.length) {
+        ast.body.push(walk());
+    }
+
+    return ast;
+}
